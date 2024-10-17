ec99752d2c3123fa66c0830bb7126ce51ec19a5d
Motors BR <dgelask8@gmail.com>
2024-10-17 11:39:06 -0300

    Adicionando lógica e condicional ao projeto

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleDarkMode()
+{
+    const html = document.documentElement;
+
+    // if (html.classList.contains('light')) {
+    //     html.classList.remove("light");
+    // } else {
+    //     html.classList.add("light");
+    // }
+
+    html.classList.toggle("light");
+}
